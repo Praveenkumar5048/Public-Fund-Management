7d03c9e202d1ae6c6e5299777e0f9343d6de3d4b
Praveenkumar5048 <praveenkumarhireboodur@gmail.com>
2025-03-22 19:30:28 +0530

    added more ideas

--- frontend/app/dashboard/fund-mangement/[id]/page.tsx
+++ frontend/app/dashboard/fund-mangement/[id]/page.tsx
@@ -7,7 +7,13 @@ export function generateStaticParams() {
     { id: '2' },
     { id: '3' },
     { id: '4' },
-    { id: '5' }
+    { id: '5' },
+    { id: '6' },
+    { id: '7' },
+    { id: '8' },
+    { id: '9' },
+    { id: '10' },
+    { id: '11' }
   ];
 }
 
